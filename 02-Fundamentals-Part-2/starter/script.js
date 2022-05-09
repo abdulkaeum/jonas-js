a4fe19c7e564b0fa4e2d963c7f7e9b5e4cc4c5b9
abdulkaeum <abdul.kaeum@hotmail.com>
2022-05-09 20:26:05 +0100

    Arrow Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -35,6 +35,7 @@ console.log(
 
 // Function Declarations vs. Expressions
 
+/*
 //Function Declarations
 function calcAge1(birthYear) {
     return 2037 - birthYear;
@@ -46,3 +47,26 @@ const calcAge2 = function (birthYear) {
     return 2037 - birthYear;
 }
 console.log(calcAge2(1991));
+*/
+
+// Arrow Functions
+const calAe3 = birthYear => 2037 - birthYear;
+console.log(calAe3(1991));
+
+const yearsUntilRetire = birthYear => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+    return retirement;
+}
+console.log(yearsUntilRetire(1991));
+
+const yearsUntilRetire2 = (birthYear, fName) => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+    return `${fName} in ${retirement}`;
+}
+console.log(yearsUntilRetire2(1991, 'bob'));
+console.log(yearsUntilRetire2(1992, 'james'));
+
+const test = () => 10 - 1;
+console.log(test());
