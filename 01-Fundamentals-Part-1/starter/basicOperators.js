ac3c63946f211cb76a0f8e74d81ebe1dc05749fb
abdulkaeum <abdul.kaeum@hotmail.com>
2022-05-09 10:33:42 +0100

    Operator Precedence

--- 01-Fundamentals-Part-1/starter/basicOperators.js
+++ 01-Fundamentals-Part-1/starter/basicOperators.js
@@ -29,5 +29,20 @@ console.log(x); // 100
 /* compare */
 
 console.log(ageJonas > ageSarah); //true // <, >=, <=, ==, ===
-
 console.log(ageJonas > 18);
+
+/* order precedence */
+/* in this case the maths is done first then the comparison */
+console.log(now - 1991 > now - 2018); //is value > value
+
+console.log(25 - 10 - 5); // left-to-right
+
+let a, b;
+a = b = 25 - 10 - 5; // a = b = 10, b = 10, a = 10
+console.log(a, b);
+
+const averageAge = ageSarah + ageJonas / 2;
+console.log(averageAge, ageSarah, ageJonas); //42 19 46 // wrong
+
+const averageAge2 = (ageSarah + ageJonas) / 2;
+console.log(averageAge2, ageSarah, ageJonas); //42 19 46 // use grouping to add first
