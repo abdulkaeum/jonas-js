ca66181f6df4110ed88ac9ffbc9fec43e78075a1
abdulkaeum <abdul.kaeum@hotmail.com>
2022-05-09 08:14:09 +0100

    Values and Variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,14 @@
 let js = "amazing";
-if (js === "amazing") alert("yes amazing");
 console.log(40 + 8 + 23 - 10);
+console.log("Abdul");
+console.log(23);
+
+let lastName = "Kaeum";
+console.log(lastName);
+console.log(lastName);
+
+let $firstname = 'Abdul'
+let $function = 'Abdul'
+let PI = 3.1415;
+
+console.log($function);
