84cf5d7918dbb0cca2ce1d132702a1b42e1a6327
abdulkaeum <abdul.kaeum@hotmail.com>
2022-05-09 19:07:35 +0100

    Strict Mode

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,10 @@
+// Strict Mode
+//'use strict';
+
+let hasDrivingsLicense = false;
+const passTest = true;
+
+if (passTest) hasDrivingLicense = true; // with 'use strict' it will provide an error otherwise not
+if (hasDrivingsLicense) console.log('i can drive');
+
+const interface = 500; // // with 'use strict' will give an order
