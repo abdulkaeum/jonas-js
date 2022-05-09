e871f569442e09d4c3c43e66ce7ceb9dff38b4b3
abdulkaeum <abdul.kaeum@hotmail.com>
2022-05-09 14:28:05 +0100

    Ternary as expression

---
+++ 01-Fundamentals-Part-1/starter/ternary.js
@@ -0,0 +1,11 @@
+// is an expression
+
+const age = 23;
+
+age >= 17 ? console.log('x') : console.log('y');
+
+let value = 'a';
+value = age >= 17 ? 'x' : 'y';
+console.log(value);
+
+console.log(`THE LETTER IS ${age >= 17 ? 'x' : 'y'}`);
