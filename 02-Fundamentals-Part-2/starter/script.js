7de646b6189bc2ae5bb14b4edf85a81b8ce8ec3b
abdulkaeum <abdul.kaeum@hotmail.com>
2022-05-09 19:28:46 +0100

    Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,6 +1,7 @@
 // Strict Mode
-//'use strict';
+'use strict';
 
+/*
 let hasDrivingsLicense = false;
 const passTest = true;
 
@@ -8,3 +9,25 @@ if (passTest) hasDrivingLicense = true; // with 'use strict' it will provide an 
 if (hasDrivingsLicense) console.log('i can drive');
 
 const interface = 500; // // with 'use strict' will give an order
+*/
+
+// Functions
+
+function logger() {
+    console.log('logger');
+}
+
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} and ${oranges}`;
+
+    return juice;
+}
+
+console.log(
+    fruitProcessor(1, 1),
+    fruitProcessor(5, 1)
+);
