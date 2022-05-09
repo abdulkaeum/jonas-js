516b24288951590de82aa1f3fd259385d4f45466
abdulkaeum <abdul.kaeum@hotmail.com>
2022-05-09 14:16:48 +0100

    Statements and Expressions

---
+++ 01-Fundamentals-Part-1/starter/statementsAndExpressions.js
@@ -0,0 +1,14 @@
+// expressions = code that make up a value
+
+/*
+3 + 4 = value i.e. returns number 7
+1991 = value i.e returns number 1991
+true && false && !false = value i.e returns a boolean
+ */
+
+// statement = code that does not produce a value by itself
+
+if(23<10) {
+    const str = '23 is bigger';
+}
+
