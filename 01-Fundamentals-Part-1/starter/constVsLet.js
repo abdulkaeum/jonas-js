aad0a18be605f462e40e20e4bc1a8ce25401b989
abdulkaeum <abdul.kaeum@hotmail.com>
2022-05-09 09:41:31 +0100

    const vs let

---
+++ 01-Fundamentals-Part-1/starter/constVsLet.js
@@ -0,0 +1,9 @@
+let age = 100;
+age = 100;
+
+const job; // error must be initialised
+const doorNumber = 2
+doorNumber = 3; //error - can't reassing a readonly variable
+
+lName = 'kaeum';
+console.log(lName) // kaeum, however it's created on the global object - bad practice
