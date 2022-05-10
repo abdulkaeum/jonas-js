344c6be8f1aff46ece2599b2ad1902921ced5a59
abdulkaeum <abdul.kaeum@hotmail.com>
2022-05-10 13:38:04 +0100

    Coding Challenge

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -290,9 +290,26 @@ for (let excercise = 1; excercise < 4; excercise++) {
 
 // while loop
 
+/*
 let dice = Math.trunc(Math.random() * 6) + 1;
 while (dice !== 6){
     console.log(`You rolled a ${dice}`);
     dice = Math.trunc(Math.random() * 6) + 1;
     if (dice === 6) console.log(`Loop is about to end...`);
+}*/
+
+// challenge
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+let tips = [];
+let totals = [];
+const calcTip = function (bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 }
+for (let i = 0; i < bills.length; i++) {
+    const tip = calcTip(bills[i]);
+    tips.push(tip);
+    totals.push(bills[i] + tip);
+}
+console.log(tips);
+console.log(totals);
