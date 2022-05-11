e61b4a2f7b35ebdbdc40da4205d60f96093ea7c6
abdulkaeum <abdul.kaeum@hotmail.com>
2022-05-11 07:19:39 +0100

    Guess my number game

--- 01-Fundamentals-Part-1/starter/constVsLet.js
+++ 01-Fundamentals-Part-1/starter/constVsLet.js
@@ -1,12 +1,12 @@
 let age = 100;
 age = 100;
 
-const job; // error must be initialised
+// const job; // error must be initialised
 const doorNumber = 2
-doorNumber = 3; //error - can't reassing a readonly variable
+// doorNumber = 3; //error - can't reassing a readonly variable
 
 // const does not allow to mutate primitive values
 // const allows to change elements within and array
 
-lName = 'kaeum';
+// lName = 'kaeum';
 console.log(lName) // kaeum, however it's created on the global object - bad practice
--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,55 @@
 'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+//document.querySelector('.number').textContent = secretNumber;
+let score = 20;
+let highScore = 0;
+
+document.querySelector('.check').addEventListener('click', function () {
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(typeof guess, guess, !guess);
+
+    if (!guess) {
+        document.querySelector('.message').textContent = '⛔ No Number!';
+    } else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = '⭐ Correct Number';
+        document.querySelector('.number').textContent = secretNumber;
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem';
+
+        if (score > highScore){
+            highScore = score;
+            document.querySelector('.highscore').textContent =  highScore;
+        }
+    } else if (guess > secretNumber) {
+        if (score > 1) {
+            score--;
+            document.querySelector('.score').textContent = score;
+            document.querySelector('.message').textContent = '⛔ Too High!';
+        } else {
+            document.querySelector('.score').textContent = 0;
+            document.querySelector('.message').textContent = '😭 You lost!';
+        }
+    } else if (guess < secretNumber) {
+        if (score > 1) {
+            score--;
+            document.querySelector('.score').textContent = score;
+            document.querySelector('.message').textContent = '⛔ Too Low!';
+        } else {
+            document.querySelector('.score').textContent = 0;
+            document.querySelector('.message').textContent = '😭 You lost!';
+        }
+    }
+});
+
+document.querySelector('.again').addEventListener('click', function (){
+    let secretNumber = Math.trunc(Math.random() * 20) + 1;
+    let score = 20;
+
+    document.querySelector('.message').textContent = 'Start guessing...'
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').style.width = '15rem';
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+    document.querySelector('body').style.backgroundColor = '#222';
+});
