ec0375f4b2cf53b7d5bb5fea77a48d5adf50917f
abdulkaeum <abdul.kaeum@hotmail.com>
2022-05-20 08:26:37 +0100

    First-Class and Higher-Order Functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// How Passing Arguments Works: Value[stack] vs. Reference[heap]
+console.log('#### - How Passing Arguments Works: Value[stack] vs. Reference[heap]');
 
 // example 1
 const flight = 'LH234';
@@ -13,9 +13,9 @@ const checkIn = function (flightNum, passenger) {
     flightNum = 'creates a new var';
     passenger.name = 'object is referenced ' + passenger.name;
 
-    passenger.passport === 1234567890
+    /*passenger.passport === 1234567890
         ? alert('Check in')
-        : alert('Wrong passport');
+        : alert('Wrong passport');*/
 };
 
 checkIn(flight, abdul); // alert('Check in')
@@ -44,3 +44,31 @@ checkIn(flight, abdul); // alert('Wrong passport');
 // passing by value / passing by reference in js
 // JS does not support passing by reference!!!! wtf
 // confusing as when passing an object to a function as per above, JS has passed by ref!
+
+console.log('#### - First-Class and Higher-Order Functions');
+// not the same thing
+//--// First-Class functions = 1st class citizens
+// they are simply just values, another 'type' of object
+// as they are values we can store them in vars or object props
+//--// Higher-Order Functions
+// First-Class functions allow us to write Higher-Order Functions
+// Higher-Order Functions = the parent function
+// Higher-Order Functions = receives a function as an argument or returns a new function
+
+// Higher-Order Functions - A Function Accepting A Callback Function:
+const oneWord = function (str){
+    return str.replace(/ /g, '').toLowerCase() // javascriptisnotthebest!
+};
+
+const upperFirstWord = function (str){
+    const [fWord, ...others] = str.split(' ');
+    return [fWord.toUpperCase(), ...others].join(' ');
+};
+// the higher order func
+const transformer = function (str, fn) {
+    console.log(`Original string: ${str}`); // Original string: JavaScript is not the best!
+    console.log(`Transform string: ${fn(str)}`); // Transform string: JAVASCRIPT is not the best!
+    console.log(`Transform by: ${fn.name}`); // Transform string: upperFirstWord
+}
+transformer('JavaScript is not the best!', upperFirstWord)
+transformer('JavaScript is not the best!', oneWord);
